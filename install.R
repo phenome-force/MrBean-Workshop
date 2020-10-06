ce474c4bc4b1070640764c5eab3d22ea865d80fa
nfahlgren <noahfahlgren@gmail.com>
2020-10-06 18:11:43 -0500

    Install golem only

--- install.R
+++ install.R
@@ -1,2 +1,3 @@
 library(devtools)
-devtools::install_github("AparicioJohan/MrBeanApp@main", dependencies=TRUE)
+#devtools::install_github("AparicioJohan/MrBeanApp@main", dependencies=TRUE)
+install.packages("golem", dependencies=TRUE, quiet=TRUE, repos=c("https://cloud.r-project.org"))
