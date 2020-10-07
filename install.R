e0246af2166ca48c3b8f2cf8476ed964177a335a
nfahlgren <noahfahlgren@gmail.com>
2020-10-06 19:31:10 -0500

    Revert "Install golem only"
    This reverts commit ce474c4bc4b1070640764c5eab3d22ea865d80fa.

--- install.R
+++ install.R
@@ -1,3 +1,2 @@
 library(devtools)
-#devtools::install_github("AparicioJohan/MrBeanApp@main", dependencies=TRUE)
-install.packages("golem", dependencies=TRUE, quiet=TRUE, repos=c("https://cloud.r-project.org"))
+devtools::install_github("AparicioJohan/MrBeanApp@main", dependencies=TRUE)
